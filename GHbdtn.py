eb6abae664150fa59895b8cdf97d38da25259d4e
pipapipapa <113211549+pipapipapa@users.noreply.github.com>
2022-10-11 13:29:43 +0300

    Update GHbdtn.py

--- GHbdtn.py
+++ GHbdtn.py
@@ -3,87 +3,87 @@ import time
 for i in range(100000):
     if i % 5 == 0 or i % 5 == 2 or i % 5 == 3:
         for k in range(5):
-            print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+            print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
-            print("\033[41m{}".format("   ") + "\033[44m{}".format("      ") + "\033[41m{}".format("   "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+            print("\033[41m{}".format("   ") + "\033[44m{}".format("      ") + "\033[41m{}".format("   "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
-            print("\033[41m{}".format("   ") + "\033[41m{}".format("      ") + "\033[41m{}".format("   "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+            print("\033[41m{}".format("   ") + "\033[41m{}".format("      ") + "\033[41m{}".format("   "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
-            print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+            print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
-            print("\033[41m{}".format("  ") + "\033[47m{}".format("        ") + "\033[41m{}".format("  "), end="\033[47m{}".format("   "))
+            print("\033[41m{}".format("  ") + "\033[0m{}".format("        ") + "\033[41m{}".format("  "), end="\033[0m{}".format("   "))
         print()
         print()
     if i % 5 == 1:
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[44m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[44m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
                 print("\033[41m{}".format("   ") + "\033[44m{}".format("      ") + "\033[41m{}".format("   "),
-                      end="\033[47m{}".format("   "))
+                      end="\033[0m{}".format("   "))
             else:
-                print("\033[44m{}".format("   ") + "\033[40m{}".format("      ") + "\033[44m{}".format("   "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[44m{}".format("   ") + "\033[40m{}".format("      ") + "\033[44m{}".format("   "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[44m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[44m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[44m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[44m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("  ") + "\033[47m{}".format("        ") + "\033[41m{}".format("  "),
-                      end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("  ") + "\033[0m{}".format("        ") + "\033[41m{}".format("  "),
+                      end="\033[0m{}".format("   "))
             else:
-                print("\033[44m{}".format("  ") + "\033[47m{}".format("        ") + "\033[44m{}".format("  "), end="\033[47m{}".format("   "))
+                print("\033[44m{}".format("  ") + "\033[0m{}".format("        ") + "\033[44m{}".format("  "), end="\033[0m{}".format("   "))
         print()
         print()
     if i % 5 == 4:
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[47m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[0m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
                 print("\033[41m{}".format("   ") + "\033[44m{}".format("      ") + "\033[41m{}".format("   "),
-                      end="\033[47m{}".format("   "))
+                      end="\033[0m{}".format("   "))
             else:
-                print("\033[47m{}".format("   ") + "\033[47m{}".format("      ") + "\033[47m{}".format("   "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[0m{}".format("   ") + "\033[0m{}".format("      ") + "\033[0m{}".format("   "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[47m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[0m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("            "), end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("            "), end="\033[0m{}".format("   "))
             else:
-                print("\033[47m{}".format("            "), end="\033[47m{}".format("   "))
-        print("\033[47m{}".format("                                                                        "))
+                print("\033[0m{}".format("            "), end="\033[0m{}".format("   "))
+        print("\033[0m{}".format("                                                                        "))
         for k in range(5):
             if k == 0 or k == 4:
-                print("\033[41m{}".format("  ") + "\033[47m{}".format("        ") + "\033[41m{}".format("  "),
-                      end="\033[47m{}".format("   "))
+                print("\033[41m{}".format("  ") + "\033[0m{}".format("        ") + "\033[41m{}".format("  "),
+                      end="\033[0m{}".format("   "))
             else:
-                print("\033[47m{}".format("  ") + "\033[47m{}".format("        ") + "\033[47m{}".format("  "), end="\033[47m{}".format("   "))
+                print("\033[0m{}".format("  ") + "\033[0m{}".format("        ") + "\033[0m{}".format("  "), end="\033[0m{}".format("   "))
         print()
         print()
         time.sleep(1.42069)
